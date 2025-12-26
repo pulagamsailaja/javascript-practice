7c1c3c377d5eb338ce2112653bc22cc83212300e
pulagamsailaja <pulagamsailaja7@gmail.com>
2025-12-26 14:51:34 -0600

    Day 1

---
+++ day1.js
@@ -0,0 +1,42 @@
+"use strict";
+// DATA TYPES
+
+let number = 10;
+let number1 = 20.125;
+console.log(number);
+console.log(number1);
+console.log(number + number1);
+
+let string='sailaja';
+console.log(string +' '+ number);
+
+let str="pulagam";
+console.log(string+ ' ' + str);
+
+let count = 5 + 7
+console.log(count)
+
+var myAge ;
+console.log(myAge)
+ 
+let islearning=true;
+console.log(islearning);
+
+let symbol = '*';
+console.log(symbol);
+
+let nullValue = null;
+console.log(nullValue);
+
+//Interaction
+//1.Alert
+//alert('Today is Day 1 of javascript');
+
+//2.Prompt
+//let js=prompt('since when you started learning?', '10 days');
+//console.log(js);
+
+//3.Confirm
+//let question =confirm(' Do you like studying');
+// console.log(question);
+
