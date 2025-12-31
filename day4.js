054f0d9818f6a22bcdc4f81c57164268b974f246
pulagamsailaja <pulagamsailaja7@gmail.com>
2025-12-31 01:11:59 -0600

    Day 4

--- day4.js
+++ day4.js
@@ -13,7 +13,45 @@ for (let i of arr) {
     console.log(i);
 }
 
-// Pattern Printing
+// Array with methods
+
+let names= ["sailaja","abhi","nani"];
+console.log(names.at(1));
+console.log(names.pop());
+console.log(names.push("chinnu"));
+names[1]="ishan";
+console.log(names);
+names.splice(2,0,"jaya");
+const newnames= names.slice(1);
+console.log(newnames);
+
+// Objects
+let user ={
+    firstname :"sailaja",
+    lastname: "pulagam",
+    age: 21,
+    isStudent: true,
+    sayMsg: function() {
+        console.log("This is fun to do");
+    }
+};
+user.projects= ["cloud chat app","online book store"];
+
+console.log(user);
+user.sayMsg();
+
+// Array of objects
+let students = [
+    { name: "Neha", age: 20, skills: ["JS", "HTML"] },
+    { name: "Manasa", age: 21, skills: ["React", "CSS"] },
+    { name: "Kavya", age: 22, skills: ["Node", "SQL"] }
+];
+
+for (let student of students) {
+    console.log(student.name, "skills:", student.skills.join(", "));
+}
+
+/* Pattern Printing
 let i,j,n=5;
 let row;
 for(i=1;i<=n;i++){
@@ -23,3 +61,4 @@ for(i=1;i<=n;i++){
     }
     console.log(row);
 }
+*/
